40ddd7bada907ed6b4b5a8d256f4206ab2f1abd8
viktordanch <viktor.danch@gmail.com>
2014-09-15 16:21:26 +0300

    update certificates

--- config/initializers/devices_push_notifications.rb
+++ config/initializers/devices_push_notifications.rb
@@ -1,5 +1,5 @@
 IceBr8kr::Application::IOS_PUSHER = Grocer.pusher(
-    certificate: "#{Rails.root}/app/assets/certificates/certificate.pem"
+    certificate: "#{Rails.root}/app/assets/certificates/certificateDev.pem"
     # passphrase:  "123456",                 # optional
     # gateway:     "gateway.sandbox.push.apple.com", # optional; See note below.
     # port:        2195,                     # optional
