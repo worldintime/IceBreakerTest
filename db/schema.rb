15ed63bfef1bcba5ebace1801723324f0dbecc6e
seniorihor <seniorigor@gmail.com>
2014-09-22 13:49:02 +0300

    Clear application.js and add users location migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140919161001) do
+ActiveRecord::Schema.define(version: 20140922103441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,12 +83,12 @@ ActiveRecord::Schema.define(version: 20140919161001) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "address"
-    t.float    "latitude"
-    t.float    "longitude"
     t.string   "facebook_uid"
     t.string   "facebook_avatar"
     t.integer  "rating",                 default: 0
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
