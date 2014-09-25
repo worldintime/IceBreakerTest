abd7df300e725753505c34e0bb2034e527559a7a
seniorihor <seniorigor@gmail.com>
2014-09-25 18:12:08 +0300

    No more need to confirm email of generated user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
-
 User.destroy_all
 puts "Populating database by users..."
 10.times do
-  FactoryGirl.create :user,
+  FactoryGirl.create :user_confirmed,
                      latitude:  rand(48.623..48.63),
                      longitude: rand(22.297..22.3)
 end
