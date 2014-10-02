570bc3cb35a8979c8f62d5ad6adc9014150b7bc1
worldintime <worldintime@gmail.com>
2014-10-02 15:56:34 +0300

    fix double initial hello

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -8,6 +8,11 @@ class Conversation < ActiveRecord::Base
 
   include ActionView::Helpers::DateHelper
 
+  def check_if_already_received(sender_id, receiver_id)
+    already_sent = Conversation.where(sender_id: receiver_id, receiver_id: sender_id,
+                                      created_at: ((Time.now - 30.minutes)..Time.now))
+  end
+
   def attr_by_default
     self.initial_viewed = false
     self.reply_viewed = true
