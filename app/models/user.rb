528eb508c3c5fea7594f03a13c9787169bd306ab
seniorihor <seniorigor@gmail.com>
2014-10-02 19:03:49 +0300

    Set up paperclip to crop avatar to center square

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, styles: { thumb: '64x64' }, default_url: '/assets/avatar.png'
+  has_attached_file :avatar, styles: { thumb: '200x200#' }, default_url: '/assets/avatar.png'
   validates_attachment :avatar, content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif'] }
   validates_presence_of :first_name, :last_name, :gender, :email, :user_name
   validates_presence_of :password, on: :create
