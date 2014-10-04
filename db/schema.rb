59b393918d38e9a8fafbd75b9a8c8e75045e5dff
boris <denwwer.c4@gmail.com>
2014-10-04 20:10:34 +0300

    fix schema; update Genfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140922154529) do
+ActiveRecord::Schema.define(version: 20140925132016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20140922154529) do
     t.boolean  "finished_viewed"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status"
   end
 
   create_table "mutes", force: true do |t|
