a648a1f83bc6e92295ac53f55f0b952ad529b917
worldintime <worldintime@gmail.com>
2014-10-31 17:43:26 +0200

    add tests for mute

---
+++ spec/models/mute_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Mute do
+  it 'should create user' do
+    mute = build :mute
+    expect(mute.save).to be true
+  end
+
+  it 'should show how many minutes are left until the end of mute' do
+    mute = Mute.create
+    expect(mute.blocked_timer).to eq 60
+  end
+
+end
+
